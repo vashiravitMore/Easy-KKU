82601c0a0544890491e3e5947e2a731b6b36a285
vashiravit.m <vashiravit.m@gmail.com>
2016-11-12 12:04:55 +0700

    Intended to SignUp

--- app/src/main/java/muenruekham/vashiravit/easykku/MainActivity.java
+++ app/src/main/java/muenruekham/vashiravit/easykku/MainActivity.java
@@ -1,14 +1,40 @@
 package muenruekham.vashiravit.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private Button signInButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget
+        signInButton = (Button) findViewById(R.id.btn_signIn);
+        signUpButton = (Button) findViewById(R.id.btn_signUp);
+
+        // Sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(
+                        MainActivity.this,
+                        SignUpActivity.class
+                ));
+            }
+        });
+
+
+
+
+    } // Main Method
+
+} // Main Class นี้คือ คลาสหลัก
 
